19115aad77758f4082cbabc53d08c2d2b4da24d7
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-13 17:04:01 +0300

    part 1

--- polybot/img_proc.py
+++ polybot/img_proc.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from matplotlib.image import imread, imsave
+import random
 
 
 def rgb2gray(rgb):
@@ -14,16 +15,22 @@ class Img:
         """
         Do not change the constructor implementation
         """
-        self.path = Path(path)
+        try:
+            self.path = Path(path)
+        except ValueError as e:
+            print('PATH not found')
         self.data = rgb2gray(imread(path)).tolist()
 
     def save_img(self):
         """
         Do not change the below implementation
         """
-        new_path = self.path.with_name(self.path.stem + '_filtered' + self.path.suffix)
-        imsave(new_path, self.data, cmap='gray')
-        return new_path
+        try:
+            new_path = self.path.with_name(self.path.stem + '_filtered' + self.path.suffix)
+            imsave(new_path, self.data, cmap='gray')
+            return new_path
+        except ValueError as e:
+            print('save image failed')
 
     def blur(self, blur_level=16):
 
@@ -46,22 +53,42 @@ class Img:
         for i, row in enumerate(self.data):
             res = []
             for j in range(1, len(row)):
-                res.append(abs(row[j-1] - row[j]))
+                res.append(abs(row[j - 1] - row[j]))
 
             self.data[i] = res
 
     def rotate(self):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+        N = len(self.data[0])
+        for i in range(N // 2):
+            for j in range(i, N - i - 1):
+                temp = self.data[i][j]
+                self.data[i][j] = self.data[N - 1 - j][i]
+                self.data[N - 1 - j][i] = self.data[N - 1 - i][N - 1 - j]
+                self.data[N - 1 - i][N - 1 - j] = self.data[j][N - 1 - i]
+                self.data[j][N - 1 - i] = temp
 
     def salt_n_pepper(self):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+        N= len(self.data)
+        for i in range (N):
+            for j in range (len(self.data[0])):
+                random_float = random.random()
+                if random_float < 0.2:
+                    self.data[i][j] = 255
+                if random_float > 0.8:
+                    self.data[i][j] = 0
 
     def concat(self, other_img, direction='horizontal'):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+        if len(self.data[0]) != len(other_img.data[0]) or len(self.data) != len(other_img.data):
+            raise RuntimeError()
+        for i in range(len(other_img.data)):
+            for j in range(len(other_img.data[i])):
+                self.data[i].append(other_img.data[i][j])
 
     def segment(self):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+        N = len(self.data)
+        for i in range(N):
+            for j in range(len(self.data[0])):
+                if self.data[i][j] > 100:
+                    self.data[i][j] = 255
+                else:
+                    self.data[i][j] = 0

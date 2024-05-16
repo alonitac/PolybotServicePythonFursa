0a23c32d47b7119d2adf82540d9f397d06aceb54
Mohmmad-amer <mohmmad3169@gmail.com>
2024-05-16 12:08:42 +0300

    part2.4

--- polybot/bot.py
+++ polybot/bot.py
@@ -25,10 +25,10 @@ class Bot:
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
 
     def send_text(self, chat_id, text):
-        self.telegram_bot_client.send_message(chat_id, text)
+        self.telegram_bot_client.send_message(chat_id, text,timeout=5)
 
     def send_text_with_quote(self, chat_id, text, quoted_msg_id):
-        self.telegram_bot_client.send_message(chat_id, text, reply_to_message_id=quoted_msg_id)
+        self.telegram_bot_client.send_message(chat_id, text, reply_to_message_id=quoted_msg_id, timeout= 5)
 
     def is_current_msg_photo(self, msg):
         return 'photo' in msg
@@ -59,7 +59,8 @@ class Bot:
 
         self.telegram_bot_client.send_photo(
             chat_id,
-            InputFile(img_path)
+            InputFile(img_path),
+            timeout=5
         )
 
     def handle_message(self, msg):
